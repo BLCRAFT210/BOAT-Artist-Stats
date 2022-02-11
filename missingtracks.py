163e5eba7c355e62db6a8df452a24eab2d36a3bc
BLCRAFT <brianslai@gmail.com>
2022-02-10 20:07:21 -0500

    update format

--- missingtracks.py
+++ missingtracks.py
@@ -3,17 +3,15 @@ import sys; args=sys.argv[1:]
 boattracks = set()
 infotracks = set()
 
-# turns each row into artist|track|genre
-
 with open(args[0]) as f:
    for line in f:
       trackdata = line.split('\t')
-      boattracks.add(trackdata[1]+'|'+trackdata[2]+'|'+trackdata[3])
+      boattracks.add(trackdata[2].lower().strip())
 
 with open(args[1]) as f:
    for line in f:
       trackdata = line.split('\t')
-      infotracks.add(trackdata[5]+'|'+trackdata[6]+'|'+trackdata[4])
+      infotracks.add(trackdata[6].lower().strip())
 
 # symmetric difference
 print(boattracks ^ infotracks)
